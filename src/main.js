51c83b80e23881a74337455d0a30bbfc4179eaa0
podluzhnyyK <podluzhnyy94@mail.ru>
2021-04-10 13:04:56 +0500

    5. Революция или эволюция? (часть 1) исправил ошибку коммита main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import {createSiteMenuTemplate} from './view/menu.js';
-import {createSiteInfoTemplate} from './view/info.js';
-import {createSiteCostTemplate} from './view/cost.js';
-import {createSiteFilterTemplate} from './view/filter.js';
-import {createSiteSortingTemplate} from './view/sorting.js';
-import {createSiteEditFormTemplate} from './view/edit-form.js';
-import {createSiteWaypointTemplate} from './view/waypoint.js';
-import {createSiteInfoAboutTripTemplate} from './view/info-about-trip.js';
-import {createSiteListTemplate} from './view/list.js';
+import TripMenuView from './view/menu.js';
+import TripSectionView from './view/info.js';
+import TripCostView from './view/cost.js';
+import TripEventsFiltersView from './view/filter.js';
+import TripEventsSortView from './view/sorting.js';
+import TripEventsEditFormView from './view/edit-form.js';
+import TripEventsItemView from './view/waypoint.js';
+import TripInfoView from './view/info-about-trip.js';
+import TripEventsListView from './view/list.js';
 import {generateTripEventsItem} from './mock/task.js';
 import { allOffers } from './mock/offer';
+import {render, RenderPosition} from './mock/utils.js';
 
 const EVENTS_COUNT = 20;
 const tripEvents = new Array(EVENTS_COUNT).fill().map(generateTripEventsItem).sort((a, b) => a.eventStartTime - b.eventStartTime);
@@ -19,30 +20,53 @@ const headerTripControls = headerTripElement.querySelector('.trip-controls');
 const pageMainContainer = document.querySelector('.page-main');
 const mainTripEventsContainer = pageMainContainer.querySelector('.trip-events');
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 // Добавляем шаблон для маршрута и стоимости
 
-render(headerTripElement, createSiteInfoTemplate(), 'afterbegin');
+render(headerTripElement, new TripSectionView().getElement(), RenderPosition.AFTERBEGIN);
 const headerTripInfoContainer = headerTripElement.querySelector('.trip-main__trip-info');
-render(headerTripInfoContainer, createSiteInfoAboutTripTemplate(tripEvents), 'beforeend');
-render(headerTripInfoContainer, createSiteCostTemplate(tripEvents), 'beforeend');
+render(headerTripInfoContainer, new TripInfoView(tripEvents).getElement(), RenderPosition.BEFOREEND);
+render(headerTripInfoContainer, new TripCostView(tripEvents).getElement(), RenderPosition.BEFOREEND);
 
 // Компоненты управления поездкой
 
-render(headerTripControls, createSiteMenuTemplate(), 'beforeend');
-render(headerTripControls, createSiteFilterTemplate(), 'beforeend');
+render(headerTripControls, new TripMenuView().getElement(), RenderPosition.BEFOREEND);
+render(headerTripControls, new TripEventsFiltersView().getElement(), RenderPosition.BEFOREEND);
 
-// Добавляем точки в список
+// trip events components
+const tripEventsListComponent = new TripEventsListView();
+render(mainTripEventsContainer, new TripEventsSortView().getElement(), RenderPosition.BEFOREEND);
+render(mainTripEventsContainer, tripEventsListComponent.getElement(), RenderPosition.BEFOREEND);
 
-render(mainTripEventsContainer, createSiteSortingTemplate(), 'beforeend');
-render(mainTripEventsContainer, createSiteListTemplate(), 'beforeend');
-const tripEventsList = mainTripEventsContainer.querySelector('.trip-events__list');
-render(tripEventsList, createSiteEditFormTemplate(tripEvents[0], allOffers), 'beforeend');
-for (let i = 1; i < EVENTS_COUNT; i++) {
-  render(tripEventsList, createSiteWaypointTemplate(tripEvents[i]), 'beforeend');
-}
+const renderTripEvent = (eventsListElement, tripEvent) => {
 
-generateTripEventsItem();
+  const tripEventComponent = new TripEventsItemView(tripEvent);
+  const tripEventEditComponent = new TripEventsEditFormView(tripEvent, allOffers);
+  const replaceCardToEditForm = () => {
+    eventsListElement.replaceChild(tripEventEditComponent.getElement(), tripEventComponent.getElement());
+  };
+
+  const replaceEditFormToCard = () => {
+    eventsListElement.replaceChild(tripEventComponent.getElement(), tripEventEditComponent.getElement());
+  };
+
+  const onEventCardRollupButtonClick = () => {
+    replaceCardToEditForm();
+  };
+
+  const onEventEditFormRollupButtonClick = () => {
+    replaceEditFormToCard();
+  };
+
+  const onEventEditFormSubmit = (evt) => {
+    evt.preventDefault();
+    replaceEditFormToCard();
+  };
+
+  tripEventComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', onEventCardRollupButtonClick);
+  tripEventEditComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', onEventEditFormRollupButtonClick);
+  tripEventEditComponent.getElement().querySelector('form').addEventListener('submit', onEventEditFormSubmit);
+
+  render(eventsListElement, tripEventComponent.getElement(), RenderPosition.BEFOREEND);
+};
+
+tripEvents.forEach((tripEvent) => renderTripEvent(tripEventsListComponent.getElement(), tripEvent));
